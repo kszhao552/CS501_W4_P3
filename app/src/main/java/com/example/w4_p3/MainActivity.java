6e1215dd0ff42d0a9c3925631281356d144d82b0
Kradon Zhao <kradonzhao@gmail.com>
2022-10-05 17:13:13 -0400

    add webview and urls that need to be loaded.

--- app/src/main/java/com/example/w4_p3/MainActivity.java
+++ app/src/main/java/com/example/w4_p3/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.w4_p3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    WebView web;
 
+    String xWeb = "https://www.ecosia.org/";
+    String yWeb = "https://www.dogpile.com/";
+    String zWeb = "https://webb.nasa.gov/";
+    String shakeWeb = "https://jumpingjaxfitness.files.wordpress.com/2010/07/dizziness.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        web = (WebView) findViewById(R.id.web);
     }
 }
