d21b00b9f16fc97b54abd10be01d64bd7a8e7983
Kradon Zhao <kradonzhao@gmail.com>
2022-10-05 17:25:52 -0400

    add accelerameter sensor

--- app/src/main/java/com/example/w4_p3/MainActivity.java
+++ app/src/main/java/com/example/w4_p3/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.w4_p3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
-    WebView web;
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private WebView web;
+    private SensorManager sensorManager;
+    private Sensor sensor;
 
     String xWeb = "https://www.ecosia.org/";
     String yWeb = "https://www.dogpile.com/";
@@ -18,5 +25,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         web = (WebView) findViewById(R.id.web);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onResume() {
+       super.onResume();
+       sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
     }
 }
