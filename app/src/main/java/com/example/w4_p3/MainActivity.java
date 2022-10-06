671e36f04bb28a777795fbb51cd4c28c623f64cc
Kradon Zhao <kradonzhao@gmail.com>
2022-10-05 21:03:45 -0400

    add shake and movement detection

--- app/src/main/java/com/example/w4_p3/MainActivity.java
+++ app/src/main/java/com/example/w4_p3/MainActivity.java
@@ -15,6 +15,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
     private Sensor sensor;
 
+    private final float NOISE = (float) 10.0;
+    private boolean flag;
+    private float lastX;
+    private float lastY;
+    private float lastZ;
+
     String xWeb = "https://www.ecosia.org/";
     String yWeb = "https://www.dogpile.com/";
     String zWeb = "https://webb.nasa.gov/";
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         web = (WebView) findViewById(R.id.web);
+        flag = false;
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -45,5 +52,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        if (!flag){
+            lastX = x;
+            lastY = y;
+            lastZ = z;
+            flag = true;
+        }
+        else {
+            float deltaX = Math.abs(lastX - x);
+            float deltaY = Math.abs(lastY - y);
+            float deltaZ = Math.abs(lastZ - z);
+
+            if (deltaX < NOISE) deltaX = (float)0.0;
+            if (deltaY < NOISE) deltaY = (float)0.0;
+            if (deltaZ < NOISE) deltaZ = (float)0.0;
+
+            lastX = x;
+            lastY = y;
+            lastZ = z;
+
+            if (deltaX != 0 && deltaX >= deltaY && deltaX >= deltaZ) {
+                web.loadUrl(xWeb);
+            } else if (deltaY != 0 && deltaY > deltaX && deltaY > deltaZ) {
+                web.loadUrl(yWeb);
+            } else if (deltaZ != 0 && deltaZ > deltaX && deltaZ > deltaY) {
+                web.loadUrl(zWeb);
+            }
+        }
     }
 }
